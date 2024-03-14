10821981a9cf5d1d66e599cb14f097bc10a4eefd
Vyacheslav <v.v.cheripko@mail.ru>
2024-03-14 10:16:41 +0300

    asda

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
-  api: 'http://10.10.1.240:3000/api/',
-  serverStaticPath: 'http://localhost:3000/images/products/'
+  api: 'http://31.133.60.135:3000/api/',
+  serverStaticPath: 'http://31.133.60.135:3000/images/products/',
+  /*  api: 'http://10.10.1.240:3000/api/',
+    serverStaticPath: 'http://10.10.1.240:3000/images/products/'*/
 };
